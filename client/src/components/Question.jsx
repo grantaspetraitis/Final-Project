ecda5832e76c106b249a02c5dd0ea51a24e5b695
grantaspetraitis <grantas157@gmail.com>
2022-07-12 18:43:46 +0300

    added more stuff to the question card

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
 const Question = (props) => {
+
+    const shortenedBody = props.data.post_body.substring(0, 100);
+
     return (
-        <Link to={`/questions/${props.data.post_id}`}>
+        <Link style={{ textDecoration: "none" }} to={`/questions/${props.data.post_id}`}>
             <div className="question-container">
                 <h1 style={{ color: "grey" }}>{props.data.post_title}</h1>
-                <p>{props.data.post_body}</p>
+                <p style={{ color: "black" }}>{shortenedBody}{props.data.post_body.length > 30 ? ' ...' : ''}</p>
+                <p>{props.data.like_amount} likes</p>
+                <p style={{ position: "relative", left: "80%" }}>By {props.data.username}</p>
             </div>
         </Link>
     );
